3614ddfc7fe2c2288a9e6d810e31dd82ef7513e0
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2026-03-13 13:49:15 +0700

    meng isi kode tombol hitung jumlah mahasiswa

--- Form1.cs
+++ Form1.cs
@@ -22,7 +22,7 @@ namespace PraktikumADO
         }
 
         //membuat method koneksi database
-        private void koneksi()
+        private void Koneksi()
         {
             conn = new SqlConnection(
                 "Data Source=LAPTOP-V3CL2RKG\\BEBEB;Initial Catalog=DBAkademikADO;Integrated Security=True" );
@@ -37,6 +37,30 @@ namespace PraktikumADO
                 conn.Open();
 
                 MessageBox.Show("Koneksi ke database berhasil");
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // Praktikum 2 - Hitung Jumlah Mahasiswa ( menggunakan ExecuteScalar() )
+        private void btnHitungMhs_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+
+                string query = "SELECT COUNT(*) FROM Mahasiswa";
+
+                cmd = new SqlCommand(query, conn);
+
+                int jumlah = (int)cmd.ExecuteScalar();
+
+                txtHasil.Text = jumlah.ToString();
 
                 conn.Close();
             }
